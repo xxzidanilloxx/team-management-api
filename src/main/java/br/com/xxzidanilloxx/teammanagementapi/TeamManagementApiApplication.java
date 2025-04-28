99fe0c512b2e927f82869129cc2ce0b8103dd330
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2025-04-28 11:46:13 -0300

    chore: estrutura base do projeto

---
+++ src/main/java/br/com/xxzidanilloxx/teammanagementapi/TeamManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.xxzidanilloxx.teammanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/xxzidanilloxx/teammanagementapi/TeamManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xxzidanilloxx.teammanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
